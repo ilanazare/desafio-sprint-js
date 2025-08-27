203c0bb1aaf07384dccfdcf719cb32f533dd82b0
IVÃ LUIS DE ALMEIDA NAZARÉ <025.026792@universidadesenaicimatec.edu.br>
2025-08-27 14:42:59 -0300

    ajusta a pagina contato

--- js/form.js
+++ js/form.js
@@ -1,14 +1,10 @@
-
-//class contato
-
 class contato {
-    constructor(nome, sobrenome, email, cpf, telefone, contato) {
+    constructor(nome, email, telefone, tipo, mensagem) {
         this.nome = nome;
-        this.sobrenome = sobrenome;
         this.email = email;
-        this.cpf = cpf;
         this.telefone = telefone;
-        this.contato = contato;
+        this.tipo = tipo;
+        this.mensagem = mensagem;
     }
     
 }
@@ -16,20 +12,13 @@ class contato {
 function Post(form) {
 
   let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
             form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
-}
+            form.elements.namedItem("tipo").value);
+            form.elements.namedItem("mensagem").value;
+            console.log("Dados do formulário", data)
+            alert(`Obrigado, ${data.nome}! Sua mensagem ${data.tipo} foi enviada com sucesso`);
+            form.reset();
 
-function Enviar() {
+  }
 
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
-
-}
